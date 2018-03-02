f68f904dc9defa05249d95d66ef8cfed7092c8dc
itwangxiang <itwangxiang@foxmail.com>
2018-03-02 10:00:06 +0800

    课程资源并发下载

--- src/app_download_course.go
+++ src/app_download_course.go
@@ -8,23 +8,21 @@ import (
 	"strconv"
 	"path"
 	"strings"
-	"sync"
 )
+
+var login string
+var password string
+
+var fileType string
+var courseId string
+var lessonId string
+var courseName string
+var couresOutPutPath string
 
 func main() {
 	fmt.Println("欢迎使用课程资源下载工具....")
 
 	var debug = false
-
-	var login string
-	var password string
-
-	var fileType string
-	var courseId string
-	var lessonId string
-	var courseName string
-
-	var outPutPath string
 
 	if debug {
 		login = ""
@@ -34,7 +32,7 @@ func main() {
 		lessonId = ""
 		courseName = courseId
 
-		outPutPath = "/Test/课件资源/"
+		couresOutPutPath = "/Test/课件资源/"
 	} else {
 		for len(login) <= 0 || len(password) <= 0 || len(fileType) <= 0 || len(courseId) <= 0 {
 			login, password, fileType, courseId, lessonId, courseName = getDownloadArags()
@@ -44,17 +42,13 @@ func main() {
 			courseName = courseId
 		}
 
-		outPutPath = utils.GetCurPath() + "课件资源" + string(os.PathSeparator)
+		couresOutPutPath = utils.GetCurPath() + "课件资源" + string(os.PathSeparator)
 	}
-
-	os.MkdirAll(outPutPath, 0777)
-
-	sitecode := "cine.web"
 
 	data := make(map[string]interface{})
 	data["phone"] = login
 	data["password"] = password
-	_, token := utils.Signin(model.Request{"", sitecode, data})
+	_, token := utils.Signin(model.Request{"", "cine.web", data})
 
 	if len(token) <= 0 || token == "" {
 		fmt.Println("no token")
@@ -66,9 +60,56 @@ func main() {
 	if lessonId != "-1" && !(len(lessonId) <= 0 || lessonId == "") {
 		data["filter"] = []string{lessonId}
 	}
-	_, rows := utils.ListWithMedias(model.Request{token, sitecode, data})
+	_, rows := utils.ListWithMedias(model.Request{token, "cine.web", data})
 
-	var files []model.DownFile
+	files := getCourseFiles(rows)
+
+	jobs := make(chan model.DownFile, len(files))
+	results := make(chan string, len(files))
+
+	for w := 1; w <= 6; w++ {
+		go downWorker(w, jobs,results)
+	}
+
+	for j := 0; j < len(files); j++ {
+		jobs <- files[j]
+	}
+	close(jobs)
+
+	for a := 1; a <= len(files); a++  {
+		fmt.Println(<-results)
+	}
+
+	fmt.Printf("一共有%d个文件，请输入任意键退出", len(files))
+	fmt.Scanln()
+}
+
+func getDownloadArags() (login, password, fileType, courseId, lessonId, courseName string) {
+	fmt.Println("请输入 User,Password,Type,CourseId,LessonId,CourseName 中间用空格隔开!")
+	fmt.Println("注释：Type:(0:压缩文件，1：原始文件) 、 LessonId：(-1:下载所有)")
+	fmt.Println("例如：user password 0 42 -1 动物农庄")
+	fmt.Scanln(&login, &password, &fileType, &courseId, &lessonId, &courseName)
+	return login, password, fileType, courseId, lessonId, courseName
+}
+
+func getUrlByType(fileType, url string) string {
+	if fileType != "1" {
+		return url
+	}
+
+	url = url[strings.Index(url, "/f/")+3:len(url)]
+
+	lastIndex := strings.LastIndex(url, "/") + 1
+
+	srcPrefix := url[0:lastIndex]
+	srcName := url[lastIndex:len(url)]
+	srcName = strings.Replace(srcName, ".png.jpg", ".png", -1)
+	srcName = strings.Replace(srcName, ".jpg.jpg", ".jpg", -1)
+
+	return "http://www.bstcine.com/f/" + srcPrefix + srcName
+}
+
+func getCourseFiles(rows []model.Chapter) (files []model.DownFile) {
 	for i := 0; i < len(rows); i++ {
 		chapterName := rows[i].Name
 		//fmt.Println("=>1. " + chapterName)
@@ -103,54 +144,13 @@ func main() {
 			}
 		}
 	}
-
-	var maxGo = 8
-	for i := 0; i < len(files); i += maxGo {
-		tmp := files[i:i+8]
-		var wg sync.WaitGroup
-		wg.Add(maxGo)
-		for i := 0; i < maxGo; i++ {
-			file := tmp[i]
-
-			if file.Path == "" {
-				wg.Done()
-				continue
-			}
-
-			go func() {
-				downPath := outPutPath+courseName+string(os.PathSeparator)+file.ChapterName+string(os.PathSeparator)+file.LessonName+string(os.PathSeparator)+file.Name
-				utils.DownloadFile(file.Path, downPath)
-				wg.Done()
-			}()
-		}
-		wg.Wait()
-	}
-
-	fmt.Printf("一共有%d个文件，请输入任意键退出", len(files))
-	fmt.Scanln()
+	return files
 }
 
-func getDownloadArags() (login, password, fileType, courseId, lessonId, courseName string) {
-	fmt.Println("请输入 User,Password,Type,CourseId,LessonId,CourseName 中间用空格隔开!")
-	fmt.Println("注释：Type:(0:压缩文件，1：原始文件) 、 LessonId：(-1:下载所有)")
-	fmt.Println("例如：user password 0 42 -1 动物农庄")
-	fmt.Scanln(&login, &password, &fileType, &courseId, &lessonId, &courseName)
-	return login, password, fileType, courseId, lessonId, courseName
-}
-
-func getUrlByType(fileType, url string) string {
-	if fileType != "1" {
-		return url
+func downWorker(id int, jobs <-chan model.DownFile,results chan<- string)  {
+	for file := range jobs {
+		downPath := couresOutPutPath+courseName+string(os.PathSeparator)+file.ChapterName+string(os.PathSeparator)+file.LessonName+string(os.PathSeparator)+file.Name
+		utils.DownloadFile(file.Path, downPath)
+		results <- "worker " + strconv.Itoa(id) + ":" + downPath+" download success..."
 	}
-
-	url = url[strings.Index(url, "/f/")+3:len(url)]
-
-	lastIndex := strings.LastIndex(url, "/") + 1
-
-	srcPrefix := url[0:lastIndex]
-	srcName := url[lastIndex:len(url)]
-	srcName = strings.Replace(srcName, ".png.jpg", ".png", -1)
-	srcName = strings.Replace(srcName, ".jpg.jpg", ".jpg", -1)
-
-	return "http://www.bstcine.com/f/" + srcPrefix + srcName
 }
