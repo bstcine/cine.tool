c46026733fdca75dc237341da2ebc989894e561a
joelin109 <joe.lin1009@gmail.com>
2018-03-15 14:35:15 +0800

    规范

--- src/conf/api.go
+++ src/conf/api.go
@@ -1,4 +1,3 @@
 package conf
 
-const APIURL_Host = "www.bstcine.com"
 const APIURL_Content_Article_List = "/api/content/article/list"
---
+++ src/conf/config.go
@@ -0,0 +1,5 @@
+package conf
+
+const Media_Host_KJ = "oss.bstcine.com"
+
+const APIURL_Host = "www.bstcine.com"
