7d494c19bb4db09ef71b48942e6273b0ee78b194
itwangxiang <itwangxiang@foxmail.com>
2018-03-15 18:12:11 +0800

    frame

--- src/conf/api.go
+++ src/conf/api.go
@@ -1,7 +1,7 @@
 package conf
 
-const APIURL_Content_Article_List = "/api/content/article/list"
-
 const APIURL_Auth_Signin = "/api/auth/signin"                                     //登录
 const APIURL_Tool_Files = "/api/tool/files"                                       //获取文件信息
 const APIURL_Content_Chapter_ListWithMedia = "/api/content/chapter/listWithMedia" //课程目录包含media
+
+const APIURL_Content_Article_List = "/api/content/article/list"
--- src/conf/config.go
+++ src/conf/config.go
@@ -1,7 +1,10 @@
 package conf
 
+//是否开发模式
+const IsDebug = false
+
+//API 配置
 const API_BASE_URL = "http://www.bstcine.com"
 const API_BASE_URL_TEST = "http://apptest.bstcine.com"
-const Media_Host_KJ = "oss.bstcine.com"
 
-const IsDebug = false
+const Media_Host_KJ = "oss.bstcine.com"
