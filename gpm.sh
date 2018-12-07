079f146e9840092136b173a670be11b8a3a8e2bf
itwangxiang <itwangxiang@foxmail.com>
2018-12-07 10:42:17 +0800

    gpm

--- gpm.sh
+++ gpm.sh
@@ -3,4 +3,6 @@
 echo "==> 正在准备第三方库";
 go get github.com/nfnt/resize;
 go get github.com/aliyun/aliyun-oss-go-sdk/oss;
+go get github.com/Luxurioust/excelize;
+go get github.com/tealeg/xlsx;
 echo "<== 第三方库准备完毕"；
